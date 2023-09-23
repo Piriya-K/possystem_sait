09b6ff40e52b21f1b6b614e2beabdf4c47e65457
howard2022 <howard.hyca@gmail.com>
2023-09-23 13:55:52 -0600

    Completed ViewModelBase.cs in ViewModels

---
+++ POS_System/ViewModels/LoginScreenViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace POS_System.ViewModels
+{
+    public class LoginScreenViewModel : ViewModelBase
+    {
+        //Fields
+        //binding the View and ViewModel
+        //Howard: skip password for now. will be completed later
+        private int _id;
+        private string _errorMessage;
+        private bool _success = true;
+
+        //Properties
+        //add propertyChange to notify the value has changed 
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                OnpropertyChanged(nameof(Id));
+            }
+
+
+        }
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnpropertyChanged(nameof(ErrorMessage));
+            }
+
+
+        }
+        public bool Success
+        {
+            get
+            {
+                return _success;
+            }
+            set
+            {
+                _success = value;
+                OnpropertyChanged(nameof(Success));
+            }
+
+
+        }
+
+        //Commands
+        public ICommand LoginCommand { get; }
+
+        //Constructor
+        public LoginScreenViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommad, CanExecuteLoginCommand);
+        }
+
+        //Here is the login validation 
+        
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+        private void ExecuteLoginCommad(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- POS_System/ViewModels/ViewModelBase.cs
+++ POS_System/ViewModels/ViewModelBase.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace POS_System.ViewModels
 {
-    internal class ViewModelBase
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //raise a event if property change
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
-}
